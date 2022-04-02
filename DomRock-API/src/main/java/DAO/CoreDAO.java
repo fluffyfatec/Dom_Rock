cfec71992967e40788d3c75ff70905817cb14e53
Luiz_Borges <88781739+luizborges17@users.noreply.github.com>
2022-04-02 02:39:42 -0300

    Add files via upload

---
+++ DomRock-API/src/main/java/DAO/CoreDAO.java
@@ -0,0 +1,35 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
+import DTO.CoreDTO;
+
+public class CoreDAO {
+	
+	  Connection conn;
+	  PreparedStatement pstm;
+	  
+	  
+public void cadastrarCore(CoreDTO objCoreDTO) {
+	 String sql = "inserir into Core(recursos) valeus (?)";
+		conn= new ConexaoDAO().conectaBD();
+		
+		 try {
+			 
+			pstm= conn.prepareStatement(sql);
+			pstm.setString(1, objCoreDTO.getRecursos());
+		
+		    
+			 
+			pstm.execute();
+			pstm.close();
+			
+			 
+		  }catch (Exception erro) {
+				 JOptionPane.showMessageDialog(null,"CoreDAO" + erro);
+			 }
+		}
+}
+
+
