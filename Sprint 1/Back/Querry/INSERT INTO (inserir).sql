c674be5dab8e5a5d9bf452da09a6b05a507ab46e
Tiago Camillo <tcamillo.tecnico@gmail.com>
2022-04-07 14:17:27 -0300

    Update Cliente

--- Sprint 1/Back/Querry/INSERT INTO (inserir).sql
+++ Sprint 1/Back/Querry/INSERT INTO (inserir).sql
@@ -43,10 +43,12 @@ VALUES ('CCR','147852','COMPRAS','PAO','OITO','NOVE','10/10/2021 12:00','09/12/2
 INSERT INTO Cliente_Funcionalidade (cf_id_cliente, cf_id_funcionalidade)
 VALUES ('1', '1');
 
-INSERT INTO Cliente(razao_social, cnpj, segmento, objetivo_negocio, entregavel_min, entregavel_possivel, datahora_cadastro, datahora_atualizacao) VALUES ('', , '', '', '', '', '', '');
+	
 
-INSERT INTO Cliente(razao_social, cnpj, segmento, objetivo_negocio, entregavel_min, entregavel_possivel, datahora_cadastro, datahora_atualizacao) VALUES ('Jorge Construtor', 95827801000131, 'Governamental', 'Objetivamente ok', 'entregavel entregavel', 'Sim Sim Entregavel', '31/03/2022', '02/04/2022');
+--INSERT INTO Cliente(razao_social, cnpj, segmento, objetivo_negocio, entregavel_min, entregavel_possivel, datahora_cadastro, datahora_atualizacao) VALUES ('Jorge Construtor', 95827801000131, 'Governamental', 'Objetivamente ok', 'Entregavel entregavel', 'Sim Sim Entregavel', '06/04/2022 15:00:20', '06/04/2022 05:48:06');
 
-INSERT INTO Cliente(razao_social, cnpj, segmento, objetivo_negocio, entregavel_min, entregavel_possivel, datahora_cadastro, datahora_atualizacao) VALUES ('Minedas House', 20737191000186, 'Atacado/Varejo', 'Objetivo do Negócio Sim', 'Minimamente entregavel talvez', 'Possivelmente entregarei isso', '01/04/2022', '05/04/2022');
+--INSERT INTO Cliente(razao_social, cnpj, segmento, objetivo_negocio, entregavel_min, entregavel_possivel, datahora_cadastro, datahora_atualizacao) VALUES ('Minedas House', 20737191000186, 'Atacado/Varejo', 'Objetivo do Negócio Sim', 'Minimamente entregavel talvez', 'Possivelmente entregarei isso', '01/04/2022 16:35:12', '05/04/2022 20:19:45');
 
-INSERT INTO Cliente(razao_social, cnpj, segmento, objetivo_negocio, entregavel_min, entregavel_possivel, datahora_cadastro, datahora_atualizacao) VALUES ('Manu Manu Tops', 78675344000198, 'Industrial', 'Objeto', 'Entregarei isso', 'Maximamente entrego isso', '31/03/2022', '02/04/2022');
+--INSERT INTO Cliente(razao_social, cnpj, segmento, objetivo_negocio, entregavel_min, entregavel_possivel, datahora_cadastro, datahora_atualizacao) VALUES ('Manu Manu Tops', 78675344000198, 'Industrial', 'Negocio Objetivo', 'Entregarei isso', 'Maximamente entrego isso', '06/04/2022 09:17:58', '06/04/2022 16:39:38');
+
+--INSERT INTO Cliente(razao_social, cnpj, segmento, objetivo_negocio, entregavel_min, entregavel_possivel, datahora_cadastro, datahora_atualizacao) VALUES ('Rei do Bife', 12345678912345, 'Atacado/Varejo', 'Objeto Negociavel', 'Entregando Possivel', 'Maximo Possivel', '06/04/2022 15:40:48', '06/04/2022 16:30:25');
