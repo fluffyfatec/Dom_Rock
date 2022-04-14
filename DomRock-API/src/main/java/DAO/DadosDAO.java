153311961e9f11c7d3c0575e4d0e91b67009e9c4
Felipe Sobral <felipesobral_@hotmail.com>
2022-04-14 01:49:09 -0300

    Setando o banco de dados em DadosDAO

--- DomRock-API/src/main/java/DAO/DadosDAO.java
+++ DomRock-API/src/main/java/DAO/DadosDAO.java
@@ -12,29 +12,53 @@ public class DadosDAO {
 	Connection conn;
 	PreparedStatement stm;
 
-	ProdutoDTO objprodutoDTO = new ProdutoDTO();
-	String produto = objprodutoDTO.getNomeProduto();
 
-	public void cadastarDados(ClienteDTO objClienteDTO) throws SQLException {
+	public void cadastrarDados(ProdutoDTO objProdutoDTO) throws SQLException {
 		String sql_id_cliente = "SELECT TOP 1 * FROM Cliente ORDER BY id_cliente DESC;";
 		Connection conn1 = new ConexaoDAO().conectaBD();
 		Statement stm1 = conn1.createStatement();
 		ResultSet result_id_cliente = stm1.executeQuery(sql_id_cliente);
-		if (produto.equals("Vox")) {
-			int idProduto = 1;
 			
-			while (result_id_cliente.next()) {
-				int id_cliente = result_id_cliente.getInt("id_cliente");
+		while (result_id_cliente.next()) {
+			int id_cliente = result_id_cliente.getInt("id_cliente");
 
-				String sql = "INSERT INTO Cliente_Produto (cp_id_cliente, dado_min, cp_id_produto) values ("
-						+ id_cliente + ", ?,"+idProduto+")";
+			String sql = "INSERT INTO Cliente_Produto (cp_id_cliente, dado_min, cp_id_produto) values ("
+						+ id_cliente + ", ?, ?)";
 				try (Connection conn = new ConexaoDAO().conectaBD();
-						PreparedStatement stm = conn.prepareStatement(sql);) {
+						PreparedStatement pstm = conn.prepareStatement(sql);) {
 
-					stm.setString(1, objClienteDTO.getDadosMin());
+					if (objProdutoDTO.getCheckvox() == 1) {
+						pstm.setString(1, objProdutoDTO.getVox());
+						pstm.setInt(2, objProdutoDTO.getCheckvox());
+						pstm.execute();
+					}
+					if (objProdutoDTO.getCheckmarketing() == 2) {
+						pstm.setString(1, objProdutoDTO.getMarketingPlanning());
+						pstm.setInt(2, objProdutoDTO.getCheckmarketing());
+						pstm.execute();
+					}
+					if (objProdutoDTO.getChecksales() == 3) {
+						pstm.setString(1, objProdutoDTO.getSalesDistribution());
+						pstm.setInt(2, objProdutoDTO.getChecksales());
+						pstm.execute();
+					}
+					if (objProdutoDTO.getCheckpricing() == 4) {
+						pstm.setString(1, objProdutoDTO.getPricing());
+						pstm.setInt(2, objProdutoDTO.getCheckpricing());
+						pstm.execute();
+					}
+					if (objProdutoDTO.getCheckoptimization() == 5) {
+						pstm.setString(1, objProdutoDTO.getOptimization());
+						pstm.setInt(2, objProdutoDTO.getCheckoptimization());
+						pstm.execute();
+					}
+					if (objProdutoDTO.getCheckmatching() == 6) {
+						pstm.setString(1, objProdutoDTO.getMatchingRisk());
+						pstm.setInt(2, objProdutoDTO.getCheckmatching());
+						pstm.execute();
+					}
 
-					stm.execute();
-					stm.close();
+					pstm.close();
 
 				} catch (SQLException e) {
 
@@ -44,5 +68,4 @@ public class DadosDAO {
 
 		}
 
-	}
 }
