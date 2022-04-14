d32a9d2252baf27e8ae086034aa77118ce4fefa0
AldrikAlvaro <aldrikfariaalvaro@gmail.com>
2022-04-13 23:51:07 -0300

    Consultas - CORE/PRODUTO/FUNCIONALIDADES

--- Sprint 1/Back/Querry/CRIAÇÃO E CONSULTA TABELAS ESCOPO.sql
+++ Sprint 1/Back/Querry/CRIAÇÃO E CONSULTA TABELAS ESCOPO.sql
@@ -16,17 +16,48 @@ SELECT * FROM Cliente_Funcionalidade;
 SELECT * FROM Cliente_Core;
 SELECT * FROM Cliente_Solucao;
 SELECT * FROM Cliente_Produto;
-
-SELECT * FROM Cliente;
 SELECT * FROM Funcionalidade;
-SELECT * FROM Core;
-SELECT * FROM Solucao;
+SELECT * FROM Cliente_Core
 SELECT * FROM Produto;
+SELECT * FROM Cliente_Core
+
+SELECT nm_produto, nm_solucao 
+FROM Produto prod
+INNER JOIN Solucao soluc
+ ON soluc.id_solucao = prod.id_solucao
 
 SELECT * 
 FROM Produto prod
 INNER JOIN Solucao soluc
   ON soluc.id_solucao = prod.id_solucao
+
+  SELECT client.razao_social, cor.recurso 
+FROM Cliente client
+INNER JOIN Cliente_Core client_cor
+  ON client_cor.cc_id_cliente = client.id_cliente
+INNER JOIN Core cor
+  ON cor.id_core = client_cor.cc_id_core
+WHERE client.id_cliente = 92
+
+----------------------------------------------------------------------------------
+
+SELECT client.razao_social, prod.nm_produto, client_prod.dado_min
+FROM Cliente client
+INNER JOIN Cliente_Produto client_prod
+  ON client_prod.cp_id_cliente = client.id_cliente
+INNER JOIN Produto prod
+  ON prod.id_produto = client_prod.cp_id_produto
+WHERE client.id_cliente = 00
+
+---------------------------------------------------------------------------------
+
+SELECT client.razao_social, func.nm_funcionalidade
+FROM Cliente client
+INNER JOIN Cliente_Funcionalidade client_func
+  ON client_func.cf_id_cliente = client.id_cliente
+INNER JOIN Funcionalidade func
+  ON func.id_funcionalidade = client_func.cf_id_funcionalidade
+WHERE client.id_cliente = 00
 
 --------------------------------------
 ----------CRIAÇÃO TABELAS-------------
