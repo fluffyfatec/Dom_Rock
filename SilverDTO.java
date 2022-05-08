7363800862c9614032ee99e8ea2e9f729b048fe5
Ana Clara Oliveira Leal <90358925+heyanaleal@users.noreply.github.com>
2022-05-07 21:47:16 -0300

    Add files via upload

---
+++ SilverDTO.java
@@ -0,0 +1,15 @@
+package DTO;
+
+public class SilverDTO {
+	
+	public String validadores;
+
+	public String getValidadores() {
+		return validadores;
+	}
+
+	public void setValidadores(String validadores) {
+		this.validadores = validadores;
+	}
+	
+}
