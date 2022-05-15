45dcedc39aefc2dfc3af4434bc6bb8e32cb70e77
Patrick Souza <patrickgabriel103f@gmail.com>
2022-05-15 15:08:32 -0300

    Organização dos diretórios

--- DomRock-API/DomRock-API/src/main/java/DTO/SimNao.java
+++
@@ -1,6 +0,0 @@
-package DTO;
-
-public enum SimNao {
-	S,N
-}
-
--- DomRock-API/DomRock-API/src/main/java/com/example/view/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example.view;
-
-public class Main {
-	
-	public static void main(String[] args) {
-		HelloApplication.main(args);
-	} 
-}
