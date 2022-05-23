020c4c66da5d25330a6bdb9aeb903973855d49cb
AldrikAlvaro <51426536+Aldrik-Alvaro@users.noreply.github.com>
2022-05-22 21:02:41 -0300

    api

--- DomRock-API/src/main/java/com/example/view/HelloApplication.java
+++ DomRock-API/src/main/java/com/example/view/HelloApplication.java
@@ -12,14 +12,14 @@ import javafx.stage.Stage;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Introducao.fxml"));
-        Scene scene = new Scene((Parent) fxmlLoader.load(), 1200, 650);
-        stage.setTitle("Sistema de Gerenciamento de Clientes - Dom Rock");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/view/Introducao.fxml"));
+        Scene scene = new Scene((Parent) fxmlLoader.load());
+        //stage.setTitle("Sistema de Gerenciamento de Clientes - Dom Rock");
         stage.setScene(scene);
         stage.setResizable(true);
         stage.setMaximized(true);
-        stage.getIcons().add(new Image("https://raw.githubusercontent.com/fluffyfatec/Front-/main/domrock.png"));
-        scene.getStylesheets().add("https://raw.githubusercontent.com/fluffyfatec/Front-/main/Styles.css");
+        //stage.getIcons().add(new Image("https://raw.githubusercontent.com/fluffyfatec/Front-/main/domrock.png"));
+        //scene.getStylesheets().add("https://raw.githubusercontent.com/fluffyfatec/Front-/main/Styles.css");
         stage.show();
 
     }
