7beaa4c0d209b8349160bbc9cfe0ad2387e98ec3
tcamillo4-ctrlz <tcamillo.tecnico@gmail.com>
2022-05-26 11:38:16 -0300

    Lógica Telas

--- DomRock-API/DomRock-API/src/main/java/modal/ConsultaUsuario.java
+++ DomRock-API/DomRock-API/src/main/java/modal/ConsultaUsuario.java
@@ -4,11 +4,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 import com.example.view.Alerts;
 
+import controller.LoginController;
 import modal.UsuarioDTO;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
 import modal.ConnectionFactory;
 
 public class ConsultaUsuario {
@@ -26,17 +30,43 @@ public class ConsultaUsuario {
 	         stm.setString(1, usuario); 
 	         stm.setString(2, senha); 
 	         rs = stm.executeQuery(); 
-	         
+	     
 	         if(rs.next()){ // se encontrou o usuario com a senha
-	        	 Alerts.display("AVISO", "LOGIN FEITO COM SUCESSO!");
+	        	 LoginController objlogon = new LoginController();
+	        	 exibiDialogoConfirmacao("LOGIN FEITO COM SUCESSO!");
+	        	 objlogon.abrirIntroducao();
 	        	return objusuarioDTO; 
 	         } else {
-	        	 Alerts.display("AVISO", "ERRO NO LOGIN");
+	        	 exibiDialogoERRO("ERRO NO LOGIN");
 	            return objusuarioDTO;
 	         }
 	 	 } catch (SQLException ex) {
 	           return null;
 	     }
 	    }
+	
+	void exibiDialogoERRO(String erro) {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Informação");
+		alert.setHeaderText(null);
+		alert.setContentText(erro);
+
+		alert.showAndWait();
+
+	}
+
+	private boolean exibiDialogoConfirmacao(String confirmacao) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Confirmação");
+		alert.setHeaderText(null);
+		alert.setContentText(confirmacao);
+
+		Optional<ButtonType> opcao = alert.showAndWait();
+
+		if (opcao.get() == ButtonType.OK)
+			return true;
+		return false;
+
+	}
 	
 }
