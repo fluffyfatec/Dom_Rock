bc4ccde881e11ae5a190773419b55f1b4db3a2a4
AldrikAlvaro <51426536+Aldrik-Alvaro@users.noreply.github.com>
2022-05-28 16:17:57 -0300

    Ajuste Consultas Banco

---
+++ Documentação/Query's/INSERT BRONZE.sql
@@ -0,0 +1,75 @@
+--MEGA GAMBIARRA MASTER
+
+
+INSERT INTO Fonte_dado(volume , frequencia , id_cliente_produto, id_origem_dado, id_formato, id_sistema)
+VALUES( '200' , '15 dias' ,
+		(SELECT cp.id_cliente_produto FROM Cliente_Produto cp
+		INNER JOIN Produto prod
+		ON prod.id_produto = cp.id_produto
+		WHERE cp.id_cliente = 1
+		AND prod.nm_produto = 'Vox')
+		,
+		(SELECT id_origem_dado FROM Origem_dado od
+		WHERE od.desc_origem = 'SFTP')
+		,
+		(SELECT fmt.id_formato FROM Formato fmt
+		WHERE fmt.formato = 'planilhas')
+		,
+		(SELECT sis.id_sistema FROM Sistema sis
+		WHERE sis.sistema = 'Outros')
+	  )
+
+/*
+		String sql = "INSERT INTO Fonte_dado(volume , frequencia , id_cliente_produto, id_origem_dado, id_formato, id_sistema)"
+				+ "VALUES( ? , ? ,"
+				+ "		(SELECT cp.id_cliente_produto FROM Cliente_Produto cp"
+				+ "		INNER JOIN Produto prod"
+				+ "		ON prod.id_produto = cp.id_produto"
+				+ "		WHERE cp.id_cliente = ?"
+				+ "		AND prod.nm_produto = ?)"
+				+ "		,"
+				+ "		(SELECT id_origem_dado FROM Origem_dado od"
+				+ "		WHERE od.desc_origem = ?)"
+				+ "		,"
+				+ "		(SELECT fmt.id_formato FROM Formato fmt"
+				+ "		WHERE fmt.formato = ?)"
+				+ "		,"
+				+ "		(SELECT sis.id_sistema FROM Sistema sis"
+				+ "		WHERE sis.sistema = ?)"
+				+ "	  )";
+
+1 - getVolume
+2 - getFrequencia
+3 - getIdCliente
+4 - getProduto
+5 - getOrigemDado
+6 - getFormato
+7 - getSistema
+
+SELECT * FROM Fonte_dado
+
+SELECT * FROM Fonte_dado
+
+SELECT cp.id_cliente_produto FROM Cliente_Produto cp
+INNER JOIN Produto prod
+ON prod.id_produto = cp.id_produto
+WHERE cp.id_cliente = 1
+AND prod.nm_produto = 'Vox'
+
+
+SELECT prod.nm_produto FROM Cliente_Produto cp
+INNER JOIN Produto prod
+ON prod.id_produto = cp.id_produto
+WHERE cp.id_cliente = 1
+*/
+
+-------------------------------------------------------------------
+--Silver
+
+SELECT * FROM Cliente_Produto cp
+		INNER JOIN Produto prod
+		ON prod.id_produto = cp.id_produto
+		WHERE cp.id_cliente = 1
+		AND prod.nm_produto = ?
+
+SELECT * FROM Validador
--- Documentação/Query's/VIEW.sql
+++ Documentação/Query's/VIEW.sql
@@ -1,3 +1,4 @@
+--VIEW CLIENTE
 DROP VIEW view_cliente;
 
 CREATE VIEW view_cliente AS
@@ -8,10 +9,11 @@ SELECT cli.id_cliente AS id_cliente,
 	   FORMAT(cli.datahora_cadastro , 'dd/MM/yyyy HH:mm:ss') as datahora_cadastro
 FROM Cliente cli;
 
-
-SELECT CONCAT(id_cliente , razao_social)  FROM Cliente
 ------------------------------------------------------------------------------------------
+--VIEW USUARIO
+
 DROP VIEW view_usuario;
+
 CREATE VIEW view_usuario AS
 SELECT usu.id_usuario AS id_usuario,
 		(CONCAT(UPPER(SUBSTRING(usu.nome, 1, 1)) , LOWER(SUBSTRING(usu.nome, 2, 80)))) AS nome,
