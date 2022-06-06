3c4e1b009b00b2939bd2eb717871ffddfaa456ff
AldrikAlvaro <51426536+Aldrik-Alvaro@users.noreply.github.com>
2022-06-05 23:09:59 -0300

    json

--- Documentação/Query's/json.sql
+++ Documentação/Query's/json.sql
@@ -1,26 +1,35 @@
-SELECT * FROM Comentario;
-SELECT * FROM Usuario;
-SELECT * FROM Validador;
+--SELECT CLIENTE PRODUTO CORE
+SELECT cli.razao_social, vcc.nm_produto, vcc.recurso FROM view_cliente_core vcc LEFT JOIN Cliente cli ON cli.id_cliente = vcc.id_cliente ORDER BY cli.razao_social, vcc.nm_produto ASC
 
-SELECT * FROM Fonte_dado;
-SELECT * FROM ClienteProduto_Core;
-SELECT * FROM ClienteProduto_Funcionalidade;
-SELECT * FROM Cliente_Produto
-ORDER BY 2 ASC
 
---INFO
-SELECT * FROM Sistema;
-SELECT * FROM Formato;
-SELECT * FROM Origem_dado;
-SELECT * FROM Core;
-SELECT * FROM Funcionalidade;
-SELECT * FROM Produto;
-SELECT * FROM Solucao;
+--SELECT CLIENTE PRODUTO FUNCIONALIDADE
+SELECT cli.razao_social, vcf.nm_produto, vcf.nm_funcionalidade  FROM view_cliente_funcionalidade vcf LEFT JOIN Cliente cli ON cli.id_cliente = vcf.id_cliente ORDER BY cli.razao_social, vcf.nm_produto ASC
 
---PEGAR
-SELECT * FROM Descritivo;
-SELECT * FROM Cliente;
 
+--SELECT BRONZE SILVER GOLD
+SELECT * FROM view_json_bronze_silver_gold ORDER BY razao_social, nm_produto ASC
+
+
+
+CREATE VIEW view_json_bronze_silver_gold AS
+
+SELECT ISNULL(cli.razao_social, 'NULL') AS razao_social, 
+	ISNULL(vb.nm_produto, 'NULL') AS nm_produto, 
+	ISNULL(vb.desc_origem, 'NULL') AS desc_origem, 
+	ISNULL(vb.formato, 'NULL') AS formato, 
+	ISNULL(vb.sistema, 'NULL') AS sistema, 
+	ISNULL(vb.volume, 'NULL') AS volume, 
+	ISNULL(vb.frequencia, 'NULL') AS frequencia, 
+	ISNULL(vb.desc_regra, 'NULL') AS desc_regra, 
+	ISNULL(vb.obrigatorio, 'NULL') AS obrigatorio, 
+	ISNULL(vb.operacao, 'NULL') AS operacao, 
+	ISNULL(vb.descritivo_operacao, 'NULL') AS descritivo_operacao 
+FROM view_bronze vb
+LEFT JOIN Cliente cli
+  ON cli.id_cliente = vb.id_cliente;
+
+
+--------------------------------------------------------------------------------------------------------
 
 SELECT id_cliente, razao_social, cnpj, segmento, datahora_cadastro
 FROM Cliente
@@ -29,40 +38,6 @@ FOR JSON AUTO
 SELECT name, state_desc, recovery_model_desc
 FROM Cliente
 FOR JSON AUTO
-
-
-
-
-SELECT * FROM Cliente_Produto
-SELECT * FROM Fonte_dado
-WHERE id_cliente_produto IN (1, 2, 3)
-
-
-SELECT * FROM Cliente cli
-
-INNER JOIN Descritivo dst
-ON dst.id_cliente = cli.id_cliente
-INNER JOIN Cliente_Produto cp
-ON cp.id_cliente = cp.id_cliente
-
-
-ORDER BY cp.id_cliente ASC
-
-
-
-
-
-
-
-
-
---
-SELECT * FROM Cliente_Produto cp
-INNER JOIN Descritivo dst
-ON dst.id_cliente = cp.id_cliente
-INNER JOIN Cliente cli
-ON dst.id_cliente = cli.id_cliente
-ORDER BY cp.id_cliente ASC
 
 
 
